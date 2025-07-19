bf3db7056638915433bcb3c4bfbf56a2121ef804
Yousef Marzouk <ymarzouk2004@gmail.com>
2025-07-19 13:58:42 -0400

    Implemented RK4 integration and expanded to non-autonomous systems.

--- ODE_solver.cpp
+++ ODE_solver.cpp
@@ -7,12 +7,15 @@
 using Eigen::VectorXd, Eigen::seqN, std::function;
 
 VectorXd solve_ode_euler(VectorXd initial_conditions, int input_parameters, int batch_size,
-	double end_time, function<VectorXd(VectorXd)>ODE);
+	double end_time, function<VectorXd(double time, VectorXd)>ODE);
+
+VectorXd solve_ode_rk4(VectorXd initial_conditions, int input_parameters, int batch_size,
+	double end_time, function<VectorXd(double time, VectorXd)>ODE);
 
 void save_samples(VectorXd samples, std::string file_path);
 
 int main(int argc, char* argv) {
-	function<VectorXd(VectorXd)> ode_function = [](VectorXd x) {
+	function<VectorXd(double time, VectorXd)> ode_function = [](double time,VectorXd x) {
 		VectorXd dxdt = -x;
 		return dxdt;
 		};
@@ -20,23 +23,46 @@ int main(int argc, char* argv) {
 	VectorXd initial_conditions = VectorXd::Zero(1);
 	initial_conditions(0) = 1.0; // Example initial condition
 
-	int input_parameters = initial_conditions.rows();
-	int batch_size = 100; // Number of time steps
+	int input_parameters = static_cast<int>(initial_conditions.rows());
+	int batch_size = 10; // Number of time steps
 	double end_time = 1.0; // Total time for the simulation
-	VectorXd samples = solve_ode_euler(initial_conditions, input_parameters, batch_size, end_time, ode_function);
+	VectorXd samples = solve_ode_rk4(initial_conditions, input_parameters, batch_size, end_time, ode_function);
 	save_samples(samples, "ode_samples.txt");
 }
 
 VectorXd solve_ode_euler(VectorXd initial_conditions, int input_parameters, int batch_size, 
-	double end_time, function<VectorXd(VectorXd)>ODE) {
+	double end_time, function<VectorXd(double time,VectorXd)>ODE) {
+	double time = 0;
 	double dt = end_time / batch_size;
 	VectorXd samples = VectorXd::Zero(batch_size * input_parameters);
 	samples.segment(0, input_parameters) = initial_conditions;
 	for (int batch = 1; batch < batch_size; batch++) {
+		time += dt;
 		int batch_start = batch * input_parameters;
 		VectorXd previous_conditions = samples(seqN(batch_start - input_parameters, input_parameters));
 		samples(seqN(batch_start, input_parameters)) = 
-			previous_conditions + ODE(previous_conditions)*dt;
+			previous_conditions + ODE(time, previous_conditions)*dt;
+	}
+	return samples;
+}
+
+VectorXd solve_ode_rk4(VectorXd initial_conditions, int input_parameters, int batch_size,
+	double end_time, function<VectorXd(double time, VectorXd)>ODE) {
+	double time = 0;
+	double dt = end_time / batch_size;
+	VectorXd samples = VectorXd::Zero(batch_size * input_parameters);
+	samples.segment(0, input_parameters) = initial_conditions;
+	for (int batch = 1; batch < batch_size; batch++) {
+		time += dt;
+		int batch_start = batch * input_parameters;
+		double half_step = 0.5 * dt;
+		VectorXd previous_conditions = samples(seqN(batch_start - input_parameters, input_parameters));
+		VectorXd k1 = ODE(time, previous_conditions);
+		VectorXd k2 = ODE(time + half_step, previous_conditions + 0.5 * k1 * dt);
+		VectorXd k3 = ODE(time + half_step, previous_conditions + 0.5 * k2 * dt);
+		VectorXd k4 = ODE(time + dt, previous_conditions + dt*k3);
+		samples(seqN(batch_start, input_parameters)) = 
+			previous_conditions + (k1 + 2 * k2 + 2 * k3 + k4) * (dt / 6.0);
 	}
 	return samples;
 }
